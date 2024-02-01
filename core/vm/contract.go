4dbd7bd97a9869027f14d4e263b4b97be5b4fc05
mattes <1240531+mattes@users.noreply.github.com>
2024-02-01 15:07:37 -0800

    feat: never subtract gas

--- core/vm/contract.go
+++ core/vm/contract.go
@@ -158,11 +158,7 @@ func (c *Contract) Caller() common.Address {
 
 // UseGas attempts the use gas and subtracts it and returns true on success
 func (c *Contract) UseGas(gas uint64) (ok bool) {
-	if c.Gas < gas {
-		return false
-	}
-	c.Gas -= gas
-	return true
+	return true // INFO(mattes) never subtract gas
 }
 
 // Address returns the contracts address
